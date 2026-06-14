0c706ce3453c94564cd7e8285b309781bf588e6b
BruceLin90620 <brucelin90620@gmail.com>
2026-06-14 14:52:34 -0400

    added process and  memory allocation

--- kernel.h
+++ kernel.h
@@ -8,6 +8,23 @@
         while (1) {}                                                            \
     } while (0)
 
+#define PROCS_MAX 8
+#define PROC_UNUSED   0
+#define PROC_RUNNABLE 1
+#define SATP_SV32 (1u << 31)
+#define PAGE_V    (1 << 0)
+#define PAGE_R    (1 << 1)
+#define PAGE_W    (1 << 2)
+#define PAGE_X    (1 << 3)
+#define PAGE_U    (1 << 4)
+
+struct process {
+    int pid;
+    int state;
+    vaddr_t sp;
+    uint8_t stack[8192];
+};
+
 struct sbiret {
     long error;
     long value;
